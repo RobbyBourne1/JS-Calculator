a18cc91cfc632d7bd04230627fce9fc0ba6bdd60
Robby Bourne <robbybourne1@gmail.com>
2017-07-17 21:14:21 -0400

    Playing around with Switch statement

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const numbers = document.getElementsByName('button')
 const output = document.querySelector('p')
 const operators = document.getElementsByName('Operator')
 const clear = document.querySelector('.buttonC')
-const equal = document.getElementsByName('Equal')
+const equal = document.querySelector('.buttonEq')
 
 // Getting the clicks to print to output
 let x = 0
@@ -32,20 +32,34 @@ clear.addEventListener('click', function(event) {
   console.log(x + y)
 })
 
-equal.forEach(solve => {
-  solve.addEventListener('click', function(event) {
-    y = output.textContent
-    if (operation === '*') {
-      output.textContent = +x * +y
-    } else if (operation === '/') {
-      output.textContent = +x / +y
-    } else if (operation === '+') {
-      output.textContent = +x + +y
-    } else if (operation === '-') {
-      output.textContent = +x - +y
-    } else {
-      x = output.textContent
-      output.textContent = x
-    }
-  })
+equal.addEventListener('click', function(event) {
+  y = output.textContent
+  // switch (operation) {
+  //   case '*'
+  //     output.textContent = +x * y
+  //     break;
+  //   case '+'
+  //     return +x + +y
+  //     break;
+  //   case '-'
+  //     return +x - +y
+  //     break;
+  //   case '/'
+  //     return +x / +y
+  //     break;
+  //   default:
+  //
+  // }
+  if (operation === '*') {
+    output.textContent = +x * +y
+  } else if (operation === '/') {
+    output.textContent = +x / +y
+  } else if (operation === '+') {
+    output.textContent = +x + +y
+  } else if (operation === '-') {
+    output.textContent = +x - +y
+  } else {
+    x = output.textContent
+    output.textContent = x
+  }
 })
